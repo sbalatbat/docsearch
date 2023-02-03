deabcda09ab3849b93fe818e524c6d42c88a6715
scbalatbat <scsb1119@gmail.com>
2023-02-02 22:42:20 -0800

    Lab activities

--- TestDocSearch.java
+++ TestDocSearch.java
@@ -17,6 +17,13 @@ public class TestDocSearch {
     URI rootPath = new URI("http://localhost/search?q=sushi");
     String expect = "Found 3 paths:\n./written_2/travel_guides/berlitz1/WhatToJapan.txt\n./written_2/travel_guides/berlitz1/WhereToJapan.txt\n./written_2/travel_guides/berlitz2/California-WhereToGo.txt";
     assertEquals(expect, h.handleRequest(rootPath));
-	}
+    }
+    @Test
+    public void testSearch1() throws URISyntaxException, IOException {
+    Handler h = new Handler("./written_2/");
+    URI rootPath = new URI("http://localhost/search?q=queer");
+    String expect = "Found 0 paths:\n";
+    assertEquals(expect, h.handleRequest(rootPath));    
+    }
 }
 
